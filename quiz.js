a4354b51d000e0d66d0becdb0592dac1b2510687
Miguel Molina <mamolina_1116@unihumboldt.edu.co>
2026-02-22 21:42:18 -0500

    Agregué quiz y juego piedra papel tijera

---
+++ quiz.js
@@ -0,0 +1,11 @@
+let puntos =0 
+
+ let respuesta1 = "a"
+ let respuesta2 = "b"
+ let respuesta3 = "c"
+
+ // respuesta 1 del usuario 
+ let respuestausuario1 = "a"
+ if (respuestausuario1 == respuesta1) {
+    puntos = puntos + 1
+ }
